652bdc86f4d4ac4cbbf3da14646560c551379aca
Ameer <lovme.amir@gmail.com>
2022-06-27 20:45:04 +0400

    redux setup done

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+
+import useStyles from './styles'
+
 
 const Form = () => {
+  const classes = useStyles();
   return (
     <div>Form</div>
   )
