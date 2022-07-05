5103c3fb1395c3327d8e6b17a96b20f16a038adf
Ameer <lovme.amir@gmail.com>
2022-07-05 19:11:42 +0400

    auth client setup in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from "react";
-import {Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import {Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import {getPosts} from './actions/posts'
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import memories from './images/logo.png'
 import useStyles from './styles'
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () =>{
     const [currentId, setCurrentId] = useState(null);
@@ -18,10 +19,7 @@ const App = () =>{
     },[currentId, dispatch])
     return(
         <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position='static' color='inherit'> 
-                <Typography className={classes.heading} variant="h2" align="center" >Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60"/>
-            </AppBar>
+            <NavBar/>
             <Grow in>
                 <Container>
                     <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}> 
--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,4 +1,3 @@
-export const LIKE = 'LIKE';
 export const CREATE = 'CREATE';
 export const UPDATE = 'UPDATE';
 export const DELETE = 'DELETE';
